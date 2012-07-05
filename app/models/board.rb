5c8ef079c7a9b5a993835bf7e59b58bff8302099
manymengofishing <mattheyj@googlemail.com>
2012-07-04 22:53:21 -0400

    Add: relationship between sites and board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,7 @@
 class Board < ActiveRecord::Base
   attr_accessible :name
 
+  has_many :sites
 
   validates :name,  :presence => true
 
--- db/migrate/20120704181959_create_sites.rb
+++ db/migrate/20120704181959_create_sites.rb
@@ -2,8 +2,10 @@ class CreateSites < ActiveRecord::Migration
   def change
     create_table :sites do |t|
       t.string :url
+      t.references :board
 
       t.timestamps
     end
   end
+  add_index :sites, :board_id
 end
